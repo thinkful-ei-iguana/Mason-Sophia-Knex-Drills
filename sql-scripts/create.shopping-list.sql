4134de8b8271b01e931da2996f14ee3b3b16438a
Mason Reichbauer <mreichba@mail.usf.edu>
2019-12-10 11:10:51 -0500

    init knex drills

---
+++ sql-scripts/create.shopping-list.sql
@@ -0,0 +1,6 @@
+--create new TYPE called grocery
+--The grocery TYPE should be an ENUM that can be one of the following possible values: Main, Snack, Lunch< Breakfast
+--create new TABLE called shopping_list w/ 6 columns (id, name, price(2 decimals), date_added(default to now()), checked(default false), category(use grocery type created))
+
+
+
